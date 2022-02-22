bd1c9e941d48cab77284b05f92a9e035383dc2a1
DZRABDemo <1156599543@qq.com>
2022-02-22 16:03:50 +0800

    this is first commit

---
+++ src/main/java/com/xufeifan/study/Test/TestDemo.java
@@ -0,0 +1,14 @@
+package com.xufeifan.study.Test;
+
+/**
+ * @author DZRABDemo
+ * @version 1.0.0
+ * @ClassName TestDemo
+ * @Description TODO
+ * @createTime 2022年02月22日 16:02:00
+ */
+public class TestDemo {
+    public static void main(String[] args) {
+
+    }
+}
